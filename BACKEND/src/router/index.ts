0b8949847c1cca4c21f3e4d79602c8e05e3f160e
waslukasz <ljwasiu@gmail.com>
2024-05-17 19:02:53 +0200

    Added project API

--- BACKEND/src/router/index.ts
+++ BACKEND/src/router/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import authentication from "./authentication";
 import users from "./users";
+import projects from "./projects";
 
 const router = express.Router();
 
 export default (): express.Router => {
   authentication(router);
   users(router);
+  projects(router);
+
   return router;
 };
